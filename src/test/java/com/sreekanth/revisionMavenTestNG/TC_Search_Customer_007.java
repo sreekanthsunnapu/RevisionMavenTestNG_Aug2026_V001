2cf6ddec9867e4eb92a879840dbd5b26ff40acf2
“sreekanth” <sreekanthsunnapu123@gmail.com>
2026-09-25 22:46:18 +0530

    added @Test annotation to method in TC_007

--- src/test/java/com/sreekanth/revisionMavenTestNG/TC_Search_Customer_007.java
+++ src/test/java/com/sreekanth/revisionMavenTestNG/TC_Search_Customer_007.java
@@ -1,11 +1,13 @@
 package com.sreekanth.revisionMavenTestNG;
 
+import org.testng.annotations.Test;
+
 public class TC_Search_Customer_007 {
 	//this is my Search Customer code
 	//	this code is added newly to check
 	// at this point of time, this code is only in my local machine
 	// not part of either my local repository or the remote repository
-	
+	@Test
 	public void searchCustomer() {
 		System.out.println("Sample code to search the customer");
 		System.out.println("Found the Customer Data!!");
