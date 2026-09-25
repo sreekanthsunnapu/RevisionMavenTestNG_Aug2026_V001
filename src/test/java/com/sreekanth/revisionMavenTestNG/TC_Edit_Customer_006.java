d6297fedba7006bdf46860ca7085e3c32a415aa4
“sreekanth” <sreekanthsunnapu123@gmail.com>
2026-09-25 21:34:10 +0530

    added Search_Customer_007 file and modified Edit_Customer_006 file

--- src/test/java/com/sreekanth/revisionMavenTestNG/TC_Edit_Customer_006.java
+++ src/test/java/com/sreekanth/revisionMavenTestNG/TC_Edit_Customer_006.java
@@ -1,2 +1,9 @@
 //This test case is added by another team member
 //to add Edit_Customer scenario 
+
+
+//I have pulled this code from the remote-repo
+//then i have refreshed my local project to see the changes. 
+
+//now i have done some modifications on the pulled code. let us see
+//let us run one more cycle of the git commands
---
+++ src/test/java/com/sreekanth/revisionMavenTestNG/TC_Search_Customer_007.java
@@ -0,0 +1,9 @@
+package com.sreekanth.revisionMavenTestNG;
+
+public class TC_Search_Customer_007 {
+	//this is my Search Customer code
+	//	this code is added newly to check
+	// at this point of time, this code is only in my local machine
+	// not part of either my local repository or the remote repository
+
+}
