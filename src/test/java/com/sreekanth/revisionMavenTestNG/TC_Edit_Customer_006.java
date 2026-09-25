55556fc5bbbf154ad62990c97804cfcd404f9f37
“sreekanth” <sreekanthsunnapu123@gmail.com>
2026-09-25 22:40:11 +0530

    modified TC_006 and TC_007

--- src/test/java/com/sreekanth/revisionMavenTestNG/TC_Edit_Customer_006.java
+++ src/test/java/com/sreekanth/revisionMavenTestNG/TC_Edit_Customer_006.java
@@ -7,3 +7,15 @@
 
 //now i have done some modifications on the pulled code. let us see
 //let us run one more cycle of the git commands
+
+package com.sreekanth.revisionMavenTestNG;
+
+import org.testng.annotations.Test;
+
+public class TC_Edit_Customer_006{
+	@Test
+	public void editCustomer() {
+		System.out.println("This is a sample testcase to work on Git & GitHub ");
+		System.out.println("And for Jenkins to access the project code from GitHub to execute it");
+	}
+}
--- src/test/java/com/sreekanth/revisionMavenTestNG/TC_Search_Customer_007.java
+++ src/test/java/com/sreekanth/revisionMavenTestNG/TC_Search_Customer_007.java
@@ -5,5 +5,10 @@ public class TC_Search_Customer_007 {
 	//	this code is added newly to check
 	// at this point of time, this code is only in my local machine
 	// not part of either my local repository or the remote repository
+	
+	public void searchCustomer() {
+		System.out.println("Sample code to search the customer");
+		System.out.println("Found the Customer Data!!");
+	}
 
 }
